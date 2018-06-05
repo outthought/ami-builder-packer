12cc0f7bb57612ee8042bfd8f202ebac6d095572
outthought <tripelipa@gmail.com>
2018-06-05 10:40:08 -0500

    Prepare for deployment to dev vpc.
    Look up data source vpc to get vpc and subnet.
    Add tags.

--- cloudformation/main.tf
+++ cloudformation/main.tf
@@ -12,33 +12,14 @@ variable ServiceName {
 
 # If the target vpc is from a cloudformation stack, then we may use the aws_cloudformation_stack data source.
 
-# variable cfn_vpc {}
-
-# data "aws_cloudformation_stack" "cfn" {
-#   name = "${var.cfn_vpc}"
-# }
-
-# resource "aws_cloudformation_stack" "amibuilder" {
-#   name         = "${var.ServiceName}"
-#   template_url = "./cloudformation/pipeline.json"
-
-#   parameters {
-#     BuilderPublicSubnet      = "${element(split(",", data.aws_cloudformation_stack.cfn.outputs.internalSubnets), 0)}"
-#     BuilderVPC               = ["${data.aws_cloudformation_stack.cfn.outputs.SPSLinuxSG}"]
-#     NotificationEmailAddress = "${var.NotificationEmailAddress}"
-#     ServiceName              = "${var.ServiceName}"
-#   }
-# }
-
-# If not a cloudformation resource, then reference a subnet.
-
-variable subnet_id {
-  # This is the sandbox VPC.
-  default = "subnet-e08e7597"
+variable cfn_vpc {
+  description = "Target VPC. e.g. dev-spsc-VPC"
+  type        = "string"
+  default     = "dev-spsc-VPC"
 }
 
-data "aws_subnet" "selected" {
-  id = "${var.subnet_id}"
+data "aws_cloudformation_stack" "cfn" {
+  name = "${var.cfn_vpc}"
 }
 
 resource "aws_cloudformation_stack" "amibuilder" {
@@ -47,12 +28,41 @@ resource "aws_cloudformation_stack" "amibuilder" {
   template_body = "${file("pipeline.yaml")}"
 
   parameters {
-    BuilderVPC               = "${data.aws_subnet.selected.vpc_id}"
-    BuilderPublicSubnet      = "${var.subnet_id}"
+    BuilderPublicSubnet      = "${element(split(",", data.aws_cloudformation_stack.cfn.outputs.internalSubnets), 0)}"
+    BuilderVPC               = "${data.aws_cloudformation_stack.cfn.outputs.VPC}"
     NotificationEmailAddress = "${var.NotificationEmailAddress}"
     ServiceName              = "${var.ServiceName}"
   }
+  tags = {
+    "sps:unit" = "techops"
+    "sps:product" = "syseng"
+    "sps:subproduct" = "amibuild"
+  }
 }
+
+# If not a cloudformation resource, then reference a subnet.
+
+# variable subnet_id {
+#   # This is the sandbox VPC.
+#   default = "subnet-e08e7597"
+# }
+
+# data "aws_subnet" "selected" {
+#   id = "${var.subnet_id}"
+# }
+
+# resource "aws_cloudformation_stack" "amibuilder" {
+#   name          = "${var.ServiceName}"
+#   capabilities  = ["CAPABILITY_IAM"]
+#   template_body = "${file("pipeline.yaml")}"
+
+#   parameters {
+#     BuilderVPC               = "${data.aws_subnet.selected.vpc_id}"
+#     BuilderPublicSubnet      = "${var.subnet_id}"
+#     NotificationEmailAddress = "${var.NotificationEmailAddress}"
+#     ServiceName              = "${var.ServiceName}"
+#   }
+# }
 
 output cfn_outputs {
   value = "${aws_cloudformation_stack.amibuilder.outputs}"
